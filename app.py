06770acbdf1200f6f900cc6c1db1fb9010f1b769
nathanwenz <nathan.silva@pm.pr.gov.br>
2026-09-23 11:09:35 -0300

    app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # 🔒 CONFIGURAÇÃO DA SENHA DE ACESSO
-SENHA_CORRETA = "18BPM2026"
+SENHA_CORRETA = "deusa"
 
 if "autenticado" not in st.session_state:
     st.session_state.autenticado = False
